4230d90b1e18f3c93b32b91843f312e0048b7c18
jscoma <jscoma@COR117-TL03>
2017-02-09 15:27:35 -0600

    123

--- Test/Test/Form1.Designer.cs
+++ Test/Test/Form1.Designer.cs
@@ -47,7 +47,7 @@
             // checkBox1
             // 
             this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(569, 48);
+            this.checkBox1.Location = new System.Drawing.Point(573, 45);
             this.checkBox1.Name = "checkBox1";
             this.checkBox1.Size = new System.Drawing.Size(80, 17);
             this.checkBox1.TabIndex = 1;
