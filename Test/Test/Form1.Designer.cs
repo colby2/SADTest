9d6349a46697022312af40b9bfc23bb9c18825a8
rkesterson1 <rkesterson1@COR117-TL04>
2017-02-09 15:30:16 -0600

    -test2

--- Test/Test/Form1.Designer.cs
+++ Test/Test/Form1.Designer.cs
@@ -64,7 +64,7 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(295, 110);
+            this.button2.Location = new System.Drawing.Point(295, 347);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(75, 23);
             this.button2.TabIndex = 3;
