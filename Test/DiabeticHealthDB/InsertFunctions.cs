7acca67c5f83e84ad603fa002c80fada56396ae3
cboren2 <cboren2@atu.edu>
2017-02-28 15:35:09 -0600

    --work on insert function for patient.cs(demographics table)

--- Test/DiabeticHealthDB/InsertFunctions.cs
+++ Test/DiabeticHealthDB/InsertFunctions.cs
@@ -9,7 +9,16 @@ namespace DiabeticHealthDB
 {
     public static class InsertFunctions
     {
-        public static int InsertIntoDemographics()
-    
+        //having issues with datetime differences between mysql and .net
+        public static int InsertIntoDemographics(string FirstName, string LastName, string DateofLastVisit, string Street, string City, string State, string Zip, string DOB, string Phone, string PrimaryInsurance, string SecondaryInsurance)
+
+        {
+            int TotalRowsInserted = 0;
+            SqlConnection conn = ComputerRepairConnectionDB.GetConnection();
+            string qry =
+               "INSERT INTO Computer(ComputerID, PurchasePrice, PurchaseDate) " +
+               "VALUES (@c, @p, @d);";
+        }
     }
 }
+
