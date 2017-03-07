3ab6bf970211a88916e828d5cf27c1ddab1949a1
jscoma <jscoma@COR117-TL05>
2017-03-07 15:40:26 -0600

    - Added a ListView for searching

--- Test/Test/Hub.Designer.cs
+++ Test/Test/Hub.Designer.cs
@@ -34,6 +34,11 @@
             this.bAdd = new System.Windows.Forms.Button();
             this.bGraphs = new System.Windows.Forms.Button();
             this.lbSearchList = new System.Windows.Forms.ListBox();
+            this.lvSearchList = new System.Windows.Forms.ListView();
+            this.FirstName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.LastName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.DOB = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.UniqueID = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
             this.SuspendLayout();
             // 
             // tbSearch
@@ -98,12 +103,52 @@
             this.lbSearchList.TabIndex = 0;
             this.lbSearchList.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
             // 
+            // lvSearchList
+            // 
+            this.lvSearchList.Activation = System.Windows.Forms.ItemActivation.OneClick;
+            this.lvSearchList.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.FirstName,
+            this.LastName,
+            this.DOB,
+            this.UniqueID});
+            this.lvSearchList.Location = new System.Drawing.Point(12, 64);
+            this.lvSearchList.Name = "lvSearchList";
+            this.lvSearchList.Size = new System.Drawing.Size(407, 97);
+            this.lvSearchList.TabIndex = 6;
+            this.lvSearchList.UseCompatibleStateImageBehavior = false;
+            this.lvSearchList.View = System.Windows.Forms.View.Details;
+            this.lvSearchList.SelectedIndexChanged += new System.EventHandler(this.listView1_SelectedIndexChanged);
+            // 
+            // FirstName
+            // 
+            this.FirstName.DisplayIndex = 0;
+            this.FirstName.Text = "First Name";
+            this.FirstName.Width = 82;
+            // 
+            // LastName
+            // 
+            this.LastName.DisplayIndex = 1;
+            this.LastName.Text = "Last Name";
+            this.LastName.Width = 91;
+            // 
+            // DOB
+            // 
+            this.DOB.DisplayIndex = 2;
+            this.DOB.Text = "DOB";
+            this.DOB.Width = 91;
+            // 
+            // UniqueID
+            // 
+            this.UniqueID.DisplayIndex = 3;
+            this.UniqueID.Text = "Unique ID";
+            // 
             // Hub
             // 
             this.AcceptButton = this.bSearch;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(854, 435);
+            this.Controls.Add(this.lvSearchList);
             this.Controls.Add(this.lbSearchList);
             this.Controls.Add(this.bGraphs);
             this.Controls.Add(this.bAdd);
@@ -125,5 +170,10 @@
         private System.Windows.Forms.Button bGraphs;
         private System.Windows.Forms.ListBox lbSearchList;
         private System.Windows.Forms.TextBox tbSearch;
+        private System.Windows.Forms.ListView lvSearchList;
+        private System.Windows.Forms.ColumnHeader FirstName;
+        private System.Windows.Forms.ColumnHeader LastName;
+        private System.Windows.Forms.ColumnHeader DOB;
+        private System.Windows.Forms.ColumnHeader UniqueID;
     }
 }
